e7885698a6904bffdf7c647dcb41b258864f0233
Brandon Dauria <bdauria@bu.edu>
2026-04-27 18:07:11 -0400

    more debugging

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "alb" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
